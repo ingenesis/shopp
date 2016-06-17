af211d119afbe931e783c8a60a33345d1ac41017
Just-m <digi-junk@hotmail.com>
2016-06-17 20:01:52 +0200

    Formatting

--- core/ui/widgets/tagcloud.php
+++ core/ui/widgets/tagcloud.php
@@ -15,37 +15,44 @@ if ( class_exists('WP_Widget') && ! class_exists('ShoppTagCloudWidget') ) {
 
 	class ShoppTagCloudWidget extends WP_Widget {
 
-	    function __construct() {
+	    function __construct () {
 	        parent::__construct(false,
-				$name = __('Shopp Tag Cloud','Shopp'),
-				array('description' => __('Popular product tags in a cloud format','Shopp'))
+				$name = Shopp::__('Shopp Tag Cloud'),
+				array('description' => Shopp::__('Popular product tags in a cloud format'))
 			);
 	    }
 
-	    function widget($args, $options) {
+	    function widget ( $args, $options ) {
 			$Shopp = Shopp::object();
-			if (!empty($args)) extract($args);
+			if ( ! empty($args) ) extract($args);
 
-			if (empty($options['title'])) $options['title'] = "Product Tags";
-			$title = $before_title.$options['title'].$after_title;
+			if ( empty($options['title']) ) $options['title'] = "Product Tags";
+			$title = $before_title . $options['title'] . $after_title;
 
-			$tagcloud = shopp('catalog','get-tagcloud',$options);
-			echo $before_widget.$title.$tagcloud.$after_widget;
+			$tagcloud = shopp('storefront.get-tagcloud', $options);
+			echo $before_widget . $title . $tagcloud . $after_widget;
 	    }
 
-	    function update($new_instance, $old_instance) {
+	    function update ( $new_instance, $old_instance ) {
 	        return $new_instance;
 	    }
 
-	    function form($options) {
+	    function form ( $options ) {
+	    	$defaults = array(
+				'title'   => '',
+				'exclude' => '',
+				);
+	    	
+			$options = array_merge($defaults, $options);	    	
 			?>
-			<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title'); ?></label>
+			<p>
+				<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title'); ?></label>
 				<input type="text" name="<?php echo $this->get_field_name('title'); ?>" id="<?php echo $this->get_field_id('title'); ?>" class="widefat" value="<?php echo $options['title']; ?>">
 			</p>
 			<p>
-				<label for="<?php echo $this->get_field_id('exclude'); ?>"><?php _e( 'Exclude' ); ?></label> <input type="text" value="<?php echo $options['exclude']; ?>" name="<?php echo $this->get_field_name('exclude'); ?>" id="<?php echo $this->get_field_id('exclude'); ?>" class="widefat" />
+				<label for="<?php echo $this->get_field_id('exclude'); ?>"><?php _e( 'Exclude' ); ?></label> <input type="text" name="<?php echo $this->get_field_name('exclude'); ?>" id="<?php echo $this->get_field_id('exclude'); ?>" class="widefat" value="<?php echo $options['exclude']; ?>" />
 				<br />
-				<small><?php _e( 'Tags, separated by commas.', 'Shopp' ); ?></small>
+				<small><?php Shopp::_e( 'Tags, separated by commas.' ); ?></small>
 			</p>
 			<?php
 	    }
