bc2617d54429446a078c7b29daa185761bf03df7
Jon Davis <jond@ingenesis.net>
2018-04-21 21:17:24 -0700

    Cleaned up some unnecessary code complexity

--- core/ui/discounts/save.php
+++ core/ui/discounts/save.php
@@ -22,22 +22,31 @@
 <?php
 
 function datefields( $name, $property ) {
-	$dateorder = Shopp::date_format_order(true);
+	$dateorder = array_keys(Shopp::date_format_order(true));
 
-	$previous = false;
-	foreach ( $dateorder as $type => $format ):
-		if ( $previous == 's' && $type[0] == 's' )
-			continue;
- 		if ( 'month' == $type ):
-			?><input type="text" name="<?php echo esc_attr($name); ?>[month]" id="<?php echo esc_attr($name); ?>-month" title="<?php Shopp::_e('Month'); ?>" size="3" maxlength="2" value="<?php echo $property > 1  ? date('n', $property) : ''; ?>" class="selectall" /><?php
-		elseif ( 'day' == $type ):
-			?><input type="text" name="<?php echo esc_attr($name); ?>[date]" id="<?php echo esc_attr($name); ?>-date" title="<?php Shopp::_e('Day'); ?>" size="3" maxlength="2" value="<?php echo $property > 1 ? date('j', $property) : ''; ?>" class="selectall" /><?php
-		elseif ( "year" == $type ):
-			?><input type="text" name="<?php echo esc_attr($name); ?>[year]" id="<?php echo esc_attr($name); ?>-year" title="<?php Shopp::_e('Year'); ?>" size="5" maxlength="4" value="<?php echo $property > 1 ? date('Y',$property) : ''; ?>" class="selectall" /><?php
-		elseif ( $type[0] == "s" ):
-			echo "/";
+	$value = '';
+	if ( $property > 1 ) {
+		$month = date('n', $property);
+		$day = date('j', $property);
+		$year = date('Y', $property);
+	}
+
+	$lastfield = false;
+	foreach ( $dateorder as $fieldtype ):
+
+		if ( 's' == $fieldtype[0] ):
+			echo 's' == $lastfield ? '' : '/';
+ 		elseif ( 'month' == $fieldtype ):
+			$value = $month;
+			?><input type="text" name="<?php echo esc_attr($name); ?>[month]" id="<?php echo esc_attr($name); ?>-month" title="<?php Shopp::_e('Month'); ?>" size="3" maxlength="2" value="<?php echo esc_attr($value); ?>" class="selectall" /><?php
+		elseif ( 'day' == $fieldtype ):
+			$value = $day;
+			?><input type="text" name="<?php echo esc_attr($name); ?>[date]" id="<?php echo esc_attr($name); ?>-date" title="<?php Shopp::_e('Day'); ?>" size="3" maxlength="2" value="<?php echo esc_attr($value); ?>" class="selectall" /><?php
+		elseif ( 'year' == $fieldtype ):
+			$value = $year;
+			?><input type="text" name="<?php echo esc_attr($name); ?>[year]" id="<?php echo esc_attr($name); ?>-year" title="<?php Shopp::_e('Year'); ?>" size="5" maxlength="4" value="<?php echo esc_attr($value); ?>" class="selectall" /><?php
 		endif;
-		$previous = $type[0];
+		$lastfield = $fieldtype[0];
 
 	endforeach;
 }
