38bd1f48a8c5f14808b56fe3f2419f303adeca10
Jon Davis <jond@ingenesis.net>
2018-05-08 18:13:05 -0700

    Fixed broken UI

--- core/ui/settings/setup.php
+++ core/ui/settings/setup.php
@@ -1,33 +1,33 @@
 <table class="form-table">
 	<tr>
 		<th scope="row" valign="top"><label for="maintenance-toggle"><?php Shopp::_e('Maintenance Mode'); ?></label></th>
-		<td><p class="toggle-setting"><input type="hidden" name="settings[maintenance]" value="off" /><input type="checkbox" name="settings[maintenance]" value="on" id="maintenance-toggle"<?php echo Shopp::array('checked' => shopp_setting('maintenance'))); ?> /><label for="maintenance-toggle"> <?php Shopp::_e('Enable maintenance mode'); ?></label></p>
+		<td><p class="toggle-setting"><input type="hidden" name="settings[maintenance]" value="off" /><input type="checkbox" name="settings[maintenance]" value="on" id="maintenance-toggle"<?php echo Shopp::inputattrs(array('checked' => shopp_setting('maintenance'))); ?> /><label for="maintenance-toggle"> <?php Shopp::_e('Enable maintenance mode'); ?></label></p>
 		<p><?php Shopp::_e('All storefront pages will display a maintenance mode message.'); ?></p></td>
 	</tr>
 
 	<tr>
 		<th scope="row" valign="top"><label for="shopping-cart-toggle"><?php Shopp::_e('Features'); ?></label></th>
 		<td>
-        <p class="toggle-setting"><input type="hidden" name="settings[shopping_cart]" value="off" /><input type="checkbox" name="settings[shopping_cart]" value="on" id="shopping-cart-toggle"<?php echo Shopp::array('checked' => shopp_setting('shopping_cart'))); ?> /><label for="shopping-cart-toggle"> <?php Shopp::_e('Enable shopping cart'); ?></label></p>
+        <p class="toggle-setting"><input type="hidden" name="settings[shopping_cart]" value="off" /><input type="checkbox" name="settings[shopping_cart]" value="on" id="shopping-cart-toggle"<?php echo Shopp::inputattrs(array('checked' => shopp_setting('shopping_cart'))); ?> /><label for="shopping-cart-toggle"> <?php Shopp::_e('Enable shopping cart'); ?></label></p>
 		<p><?php Shopp::_e('Uncheck this to disable the shopping cart and checkout. Useful for catalog-only sites.'); ?></p>
-        
-        <p class="toggle-setting"><input type="hidden" name="settings[shipping]" value="off" /><input type="checkbox" name="settings[shipping]" value="on" id="shipping-toggle"<?php echo Shopp::array('checked' => shopp_setting('shipping'))); ?> /><label for="shipping-toggle"> <?php Shopp::_e('Enable shipping costs'); ?></label></p>
+
+        <p class="toggle-setting"><input type="hidden" name="settings[shipping]" value="off" /><input type="checkbox" name="settings[shipping]" value="on" id="shipping-toggle"<?php echo Shopp::inputattrs(array('checked' => shopp_setting('shipping'))); ?> /><label for="shipping-toggle"> <?php Shopp::_e('Enable shipping costs'); ?></label></p>
     	<p><?php Shopp::_e('Enables shipping cost calculations. Disable if you are exclusively selling intangible products.'); ?></p>
-        
-        <p class="toggle-setting"><input type="hidden" name="settings[taxes]" value="off" /><input type="checkbox" name="settings[taxes]" value="on" id="taxes-toggle"<?php echo Shopp::array('checked' => shopp_setting('taxes'))); ?> /><label for="taxes-toggle"> <?php Shopp::_e('Enable tax calculations'); ?></label></p>
+
+        <p class="toggle-setting"><input type="hidden" name="settings[taxes]" value="off" /><input type="checkbox" name="settings[taxes]" value="on" id="taxes-toggle"<?php echo Shopp::inputattrs(array('checked' => shopp_setting('taxes'))); ?> /><label for="taxes-toggle"> <?php Shopp::_e('Enable tax calculations'); ?></label></p>
         <p><?php Shopp::_e('Disable if you are exclusively selling non-taxable items.'); ?></p>
-        
-        <p class="toggle-setting"><input type="hidden" name="settings[inventory]" value="off" /><input type="checkbox" name="settings[inventory]" value="on" id="inventory-toggle"<?php echo Shopp::array('checked' => shopp_setting('inventory'))); ?> /><label for="inventory-toggle"> <?php Shopp::_e('Enable inventory tracking'); ?></label></p>
+
+        <p class="toggle-setting"><input type="hidden" name="settings[inventory]" value="off" /><input type="checkbox" name="settings[inventory]" value="on" id="inventory-toggle"<?php echo Shopp::inputattrs(array('checked' => shopp_setting('inventory'))); ?> /><label for="inventory-toggle"> <?php Shopp::_e('Enable inventory tracking'); ?></label></p>
         <p><?php Shopp::_e('Disable if you are exclusively selling intangible products or not keeping track of product stock.'); ?></p>
         </td>
 	</tr>
-    
+
 	<tr>
 		<th scope="row" valign="top"><label for="dashboard-toggle"><?php Shopp::_e('Dashboard Widgets'); ?></label></th>
 		<td><p class="toggle-setting"><input type="hidden" name="settings[dashboard]" value="off" /><input type="checkbox" name="settings[dashboard]" value="on" id="dashboard-toggle"<?php if (shopp_setting('dashboard') == "on") echo ' checked="checked"'?> /><label for="dashboard-toggle"> <?php Shopp::_e('Enable dashboard widgets'); ?></label></p>
 		<p><?php Shopp::_e('Check this to display store performance metrics and more on the WordPress Dashboard.'); ?></p></td>
 	</tr>
-    
+
 	<tr>
 		<th scope="row" valign="top"><label for="base_operations"><?php Shopp::_e('Base of Operations'); ?></label></th>
 		<td><select name="settings[country]" id="base_operations">
@@ -37,7 +37,7 @@
 			<select name="settings[state]" id="base_operations_zone"<?php if ( empty($statesmenu) ): ?>disabled="disabled" class="hide-if-no-js"<?php else: ?>  placeholder="<?php Shopp::_e('Select your %s&hellip;', strtolower(ShoppBaseLocale()->division())); ?>"<?php endif; ?>>
 				<?php echo $statesmenu; ?>
 			</select>
-			
+
 			<p><?php Shopp::_e('Select your primary business location.'); ?></p>
 			<strong><?php Shopp::_e('Currency'); ?>: </strong><?php echo Shopp::money(1000.00); ?>
 			<?php if ( shopp_setting_enabled('tax_inclusive') ): ?><strong>(+<?php echo strtolower(Shopp::__('Tax included')); ?>)</strong><?php endif; ?>
@@ -56,16 +56,16 @@
 							if ( $even ) $classes[] = 'odd';
 					?>
 						<li<?php if ( ! empty($classes) ) echo ' class="' . join(' ', $classes) . '"'; ?>><label for="market-<?php echo $iso; ?>" accesskey="<?php echo substr($iso, 0, 1); ?>"><input type="checkbox" name="settings[target_markets][<?php echo $iso; ?>]" value="<?php echo $country; ?>" id="market-<?php echo $iso; ?>" checked="checked" /><?php echo $country; ?></label></li>
-					<?php $even = !$even; 
-						endforeach; 
+					<?php $even = !$even;
+						endforeach;
 						foreach ($countries as $iso => $country):
 							$classes = array();
 							if ( $even ) $classes[] = 'odd';
 						?>
 					<?php if ( ! in_array($country, $targets) ): ?>
 					<li<?php if ( ! empty($classes) ) echo ' class="' . join(' ', $classes) . '"'; ?>><label for="market-<?php echo $iso; ?>" accesskey="<?php echo substr($iso, 0, 1); ?>"><input type="checkbox" name="settings[target_markets][<?php echo $iso; ?>]" value="<?php echo $country; ?>" id="market-<?php echo $iso; ?>" /><?php echo $country; ?></label></li>
-					<?php $even = !$even; 
-							endif; 
+					<?php $even = !$even;
+							endif;
 						endforeach; ?>
 				</ul>
 			</div>
@@ -96,7 +96,5 @@
 <p class="submit"><input type="submit" class="button-primary" name="save" value="<?php Shopp::_e('Save Changes'); ?>" /></p>
 
 <script type="text/javascript">
-/* <![CDATA[ */
 	var zones_url = '<?php echo $zones_ajaxurl; ?>';
-/* ]]> */
 </script>
